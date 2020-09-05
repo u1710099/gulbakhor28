17d79b049c291a0cb69ff209250a350a640ce646
u1710099 <g.mirzalieva@student.inha.uz>
2020-09-06 01:39:18 +0500

    committed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,13 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this, "Button was clicked! ", Toast.LENGTH_SHORT).show()
 
+        }
+        btnSendMsgToNextActivity.setOnClickListener{
+         val message: String = etUserMessage.text.toString()
+            Toast.makeText(this,message, Toast.LENGTH_SHORT ).show()
+
+            val intent = Intent(this,SecondActivity::class.java)
+         startActivity(intent)
         }
     }
 }
