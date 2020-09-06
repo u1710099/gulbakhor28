210c5af419a1bfad6237910ac4f1b6a933391b53
u1710099 <g.mirzalieva@student.inha.uz>
2020-09-06 11:12:10 +0500

    committed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -20,10 +20,24 @@ class MainActivity : AppCompatActivity() {
         }
         btnSendMsgToNextActivity.setOnClickListener{
          val message: String = etUserMessage.text.toString()
-            Toast.makeText(this,message, Toast.LENGTH_SHORT ).show()
+
 
             val intent = Intent(this,SecondActivity::class.java)
+
+            intent.putExtra("user message",message)
          startActivity(intent)
+        }
+
+        btnShareToOtherApps.setOnClickListener{
+            val message: String = etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type="text/plain"
+
+            startActivity(Intent.createChooser(intent,"share to: "))
         }
     }
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -2,12 +2,20 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity:AppCompatActivity(){
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val bundle: Bundle? = intent.extras
+        val msg = bundle!!.getString("user_message")
+        Toast.makeText(this, msg,Toast.LENGTH_SHORT).show()
+
+        txvUserMessage.text=msg
     }
 }
